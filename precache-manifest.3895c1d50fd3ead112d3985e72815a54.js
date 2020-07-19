4955561f298b3e5f9c6e14ca11c5eedac66e0f4a
Yuriy13202 <ascendo@list.ru>
2020-07-19 15:41:02 +0700

    Updates

---
+++ precache-manifest.3895c1d50fd3ead112d3985e72815a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a82ce4cc7078359890aa7562ecf2ec1",
+    "url": "/star-db/index.html"
+  },
+  {
+    "revision": "61aa57c075fef7b6c8f3",
+    "url": "/star-db/static/css/main.ecb0e79f.chunk.css"
+  },
+  {
+    "revision": "9c69b54c2dd4700d2c81",
+    "url": "/star-db/static/js/2.cc0336d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-db/static/js/2.cc0336d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61aa57c075fef7b6c8f3",
+    "url": "/star-db/static/js/main.7525bb71.chunk.js"
+  },
+  {
+    "revision": "0b5720943e7f3b94b5cc",
+    "url": "/star-db/static/js/runtime-main.294e48e2.js"
+  }
+]);
